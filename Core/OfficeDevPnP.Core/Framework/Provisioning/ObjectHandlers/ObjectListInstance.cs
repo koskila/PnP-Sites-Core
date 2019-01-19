60dc79c84c3fac1432eea78b333260337c30a7f3
koskila <posti@koskila.net>
2019-01-19 23:17:05 +0200

    Possible fix for #2038
    Fixes a bug where site columns would not be added in the provisioning template if content types are enabled on a list and site columns are not added to them as well.

--- Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectListInstance.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectListInstance.cs
@@ -2298,12 +2298,9 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
                 if (siteColumn != null)
                 {
                     var addField = true;
-                    if (siteList.ContentTypesEnabled && contentTypeFields.FirstOrDefault(c => c.Id == field.Id) == null)
+                    if (siteList.ContentTypesEnabled && contentTypeFields.FirstOrDefault(c => c.Id == field.Id) != null)
                     {
-                        if (contentTypeFields.FirstOrDefault(c => c.Id == field.Id) == null)
-                        {
                             addField = false;
-                        }
                     }
 
                     if (siteColumn.DefaultValue != field.DefaultValue)
